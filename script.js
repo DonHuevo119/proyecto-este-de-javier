8182661f2c281c0323795a94239b5853dff3db77
DonHuevo119 <1193834006huevo@gmail.com>
2024-02-01 13:00:55 +0100

    hola

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ function error() {
         document.querySelector('.intentos').innerText = "You have reached the maximum number of login attempts the system is locked"
         document.querySelector('.ventanaemergente').style.display = "flex"
         document.querySelector('.alaverga').style.display = "flex"
-        document.querySelector('.signin').style.backgroundColor = "#091717";
-        document.querySelector('.signin').style.cursor = "alias";
+        document.querySelector('.signin').style.backgroundColor = "#091717"
+        document.querySelector('.signin').style.cursor = "alias"
         document.querySelector('.lock').style.visibility = "visible"
         document.querySelector('.ventanaemergente2').style.display = "flex"
     }
@@ -36,17 +36,29 @@ function abrirFormulario() {
 }
 
 
-  function comprobarFormulario() {
-    let texto = document.getElementById("primerInput").textContent
-    console.log(texto)
-    var avisoElement = document.querySelector('.aviso');
-    avisoElement.style.display = "block";
+function comprobarFormulario() {
+    let texto = document.getElementById("quegracioso").value
+    let texto2 = document.getElementById("quegracioso2").value
+    if (texto === "admin" && texto2 === "admin") {
+        document.querySelector('.ventanaemergente3').style.display = "none"
+        document.querySelector('.ventanaemergente2').style.display = "none"
+        document.querySelector('.signin').style.backgroundColor = "#20808D"
+        document.querySelector('.signin').style.cursor = "pointer"
+        document.querySelector('.lock').style.visibility = "hidden"
+        document.querySelector('.ar').style.display = "block"
+        document.getElementById("quegracioso").value = ""
+        document.getElementById("quegracioso2").value = ""
+        contador = 0
+    } else {
+        var avisoElement = document.querySelector('.aviso');
+        avisoElement.style.display = "block";
 
-    // Añadir la clase shakeX después de mostrar el elemento
-    avisoElement.classList.add('shakeX');
+        // Añadir la clase shakeX después de mostrar el elemento
+        avisoElement.classList.add('shakeX');
 
-    // Eliminar la clase shakeX después de la animación
-    setTimeout(function() {
-        avisoElement.classList.remove('shakeX');
-    }, 500); // Tiempo igual a la duración de la animación
+        // Eliminar la clase shakeX después de la animación
+        setTimeout(function () {
+            avisoElement.classList.remove('shakeX');
+        }, 500); // Tiempo igual a la duración de la animación
+    }
 }
